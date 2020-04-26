094b53945cb12f2550dd3cdd29ff9bc5d88994a1
Edmundo Josue Sanchez Mendez <edmundoelpro1@gmail.com>
2020-04-25 21:30:32 -0500

    Iniciando crear estados financieros
    Ya volvi a hacer el proyecto jsjsjsjs, empece a generar el JSON para mandar al servidor, despues de ello en la parte del servidor sera en donde se genararan los PDF y se mandaran de regreso

--- react-adoo/src/components/Cuentas.js
+++ react-adoo/src/components/Cuentas.js
@@ -7,14 +7,21 @@ import Box from 'react-bulma-components/lib/components/box';
 import Button from 'react-bulma-components/lib/components/button';
 import Section from 'react-bulma-components/lib/components/section';
 import axios from 'axios';
-import List from 'react-bulma-components/lib/components/list'
+import List from 'react-bulma-components/lib/components/list';
+import SweetAlert from 'react-bootstrap-sweetalert';
 class Cuentas extends Component {
-
-    state = ({
-        CuentasExistentes: localStorage.getItem('CuentasExistentes') ? JSON.parse(localStorage.getItem("CuentasExistentes")) : [],
-        select: localStorage.getItem("select") ? localStorage.getItem("select") : '...',
-        CuentasNExistentes: localStorage.getItem('CuentasNExistentes') ? JSON.parse(localStorage.getItem("CuentasNExistentes")) : [],
-    })
+    constructor(props) {
+        super(props);
+        this.state={
+            CuentasExistentes: localStorage.getItem('CuentasExistentes') ? JSON.parse(localStorage.getItem("CuentasExistentes")) : [],
+            select: localStorage.getItem("select") ? localStorage.getItem("select") : '...',
+            CuentasNExistentes: localStorage.getItem('CuentasNExistentes') ? JSON.parse(localStorage.getItem("CuentasNExistentes")) : [],
+            MovimientosIzquierda: localStorage.getItem('MovimientosIzq') ? JSON.parse(localStorage.getItem("MovimientosIzq")) : [],
+            MovimientosDerecha: localStorage.getItem('MovimientosDer') ? JSON.parse(localStorage.getItem("MovimientosDer")) : [],   
+            alert:null    
+        };
+        this.GenerarEstados = this.GenerarEstados.bind(this);
+    }
     componentDidMount() {
         let { ValorCuenta } = this.props.location.state
         const sel = { idCM: ValorCuenta }
@@ -38,6 +45,39 @@ class Cuentas extends Component {
             }
         }
         ValorCuenta = -1
+    }
+    hideAlert = () => {
+        this.setState({
+            alert: null,
+        });
+    }
+    GenerarEstados() {
+        let Condicion1= this.state.MovimientosIzquierda;
+        let Condicion2= this.state.MovimientosDerecha;
+        if(Condicion1.length===0&&Condicion2.length===0){
+            this.setState({alert: (<SweetAlert warning title="No tiene movimientos registrados" onConfirm={this.hideAlert} onCancel={this.hideAlert}/>)})
+        }else
+            Condicion1 = this.state.MovimientosIzquierda.filter(item=>item.Check===false);
+            Condicion2 = this.state.MovimientosDerecha.filter(item=>item.Check===false);
+            if(Condicion1.length===0&&Condicion2.length===0){
+                Condicion1= this.state.MovimientosIzquierda;
+                Condicion2= this.state.MovimientosDerecha;
+                let CuentasExist = this.state.CuentasExistentes;
+                let CuentasNExist = this.state.CuentasNExistentes.length===0? []:this.state.CuentasNExistentes;
+                let Metodo = this.state.select;
+                let JSONC=[];
+                let LongJSONC=0;
+                for(let i=0;i<CuentasExist.length;i++)
+                    JSONC[i]={id:i,NombreCuenta:CuentasExist[i].NombreCuenta};
+                LongJSONC = JSONC.length
+                for(let i=0;i<CuentasNExist.length;i++){
+                    JSONC[i]={id:i,NombreCuenta:CuentasNExist[i].NombreCuenta};
+                    LongJSONC=LongJSONC+1;
+                }
+                let JSONF = {"Cuentas":JSONC,"MovimientosIzq":Condicion1,"MovimientosDer":Condicion2,"Metodo":Metodo}
+                console.log(JSONF)
+            }else
+                this.setState({alert: (<SweetAlert warning title="Hay movimientos que aun no ha terminado" onConfirm={this.hideAlert} onCancel={this.hideAlert}/>)})
     }
     render() {
         return (
@@ -90,14 +130,8 @@ class Cuentas extends Component {
                         </Columns>
                         <Box>
                             <Button.Group>
-                                <Button renderAs="button" color="success">
-                                    Balance General
-                                </Button>
-                                <Button renderAs="button" color="success">
-                                    Balanza de Comprobación
-                                </Button>
-                                <Button renderAs="button" color="success">
-                                    Estado de Resultados
+                                <Button renderAs="button" color="success" onClick={this.GenerarEstados}>
+                                    Generar Estados Financieros
                                 </Button>
                                 <Link to='addcuenta'>
                                     < Button renderAs="button" color="success" >
@@ -114,8 +148,9 @@ class Cuentas extends Component {
                             </Link>
                         </p>
                     </Content>
-                </div >
-            </Section >
+                    {this.state.alert}
+                </div>
+            </Section>
         )
     }
 }
