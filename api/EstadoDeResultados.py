7ddf7303de8d2003ab6d6ce31eb6ff0d18cb1084
Edmundo Josue Sanchez Mendez <edmundoelpro1@gmail.com>
2020-04-28 22:16:03 -0500

    Se finalizo estado de resultados
    Se espera que el codigo para la generacion del ultimo estado sea a mas tardar el dia 5 de mayo, para luego pasar al montaje en el servidor local y poco despues pasar al montaje en hosting

--- api/EstadoDeResultados.py
+++ api/EstadoDeResultados.py
@@ -1,15 +1,13 @@
 # List of Lists
-JSONCuentas=[{'id': 0, 'NombreCuenta': 'Almacen'}, {'id': 1, 'NombreCuenta': 'Ventas'}, {'id': 2, 'NombreCuenta': 'Costo de ventas'}, {'id': 0, 'NombreCuenta': 'Caja'}]
-JSONMovIzq=[{'id': 0, 'NombreCuenta': 'Almacen', 'NumMovimiento': '1', 'Cantidad': '200', 'Check': True}, {'id': 1, 'NombreCuenta': 'Costo de ventas', 'NumMovimiento': '2', 'Cantidad': '1200', 'Check': True}, {'id': 2, 'NombreCuenta': 'Caja', 'NumMovimiento': '3', 'Cantidad': '50000', 'Check': True}]
-JSONMovDer=[{'id': 0, 'NombreCuenta': 'Ventas', 'NumMovimiento': '1', 'Cantidad': '200', 'Check': True}, {'id': 1, 'NombreCuenta': 'Almacen', 'NumMovimiento': '2', 'Cantidad': '1200', 'Check': True}, {'id': 2, 'NombreCuenta': 'Costo de ventas', 'NumMovimiento': '3', 'Cantidad': '50000', 'Check': True}]
+JSONCuentas=[{'id': 0, 'NombreCuenta': 'Almacen'}, {'id': 1, 'NombreCuenta': 'Ventas'}, {'id': 2, 'NombreCuenta': 'Costo de ventas'}, {'id': 0, 'NombreCuenta': 'Caja'}, {'id': 1, 'NombreCuenta': 'Gastos financieros'}, {'id': 2, 'NombreCuenta': 'Gasto de venta'}, {'id': 3, 'NombreCuenta': 'Gasto de administración'}, {'id': 4, 'NombreCuenta': 'Otros gastos'}]
+JSONMovIzq=[{'id': 0, 'NombreCuenta': 'Almacen', 'NumMovimiento': '1', 'Cantidad': '366000', 'Check': True}, {'id': 1, 'NombreCuenta': 'Caja', 'NumMovimiento': '2', 'Cantidad': '115000', 'Check': True}, {'id': 2, 'NombreCuenta': 'Caja', 'NumMovimiento': '3', 'Cantidad': '230000', 'Check': True}, {'id': 3, 'NombreCuenta': 'Costo de ventas', 'NumMovimiento': '4', 'Cantidad': '115000', 'Check': True}, {'id': 4, 'NombreCuenta': 'Gastos financieros', 'NumMovimiento': '5', 'Cantidad': '4000', 'Check': True}, {'id': 5, 'NombreCuenta': 'Gasto de venta', 'NumMovimiento': '6', 'Cantidad': '83500', 'Check': True}, {'id': 6, 'NombreCuenta': 'Gasto de administración', 'NumMovimiento': '7', 'Cantidad': '78000', 'Check': True}, {'id': 7, 'NombreCuenta': 'Otros gastos', 'NumMovimiento': '8', 'Cantidad': '7000', 'Check': True}]
+JSONMovDer=[{'id': 0, 'NombreCuenta': 'Caja', 'NumMovimiento': '1', 'Cantidad': '366000', 'Check': True}, {'id': 1, 'NombreCuenta': 'Almacen', 'NumMovimiento': '2', 'Cantidad': '115000', 'Check': True}, {'id': 2, 'NombreCuenta': 'Ventas', 'NumMovimiento': '3', 'Cantidad': '230000', 'Check': True}, {'id': 3, 'NombreCuenta': 'Caja', 'NumMovimiento': '4', 'Cantidad': '115000', 'Check': True}, {'id': 4, 'NombreCuenta': 'Caja', 'NumMovimiento': '5', 'Cantidad': '4000', 'Check': True}, {'id': 5, 'NombreCuenta': 'Caja', 'NumMovimiento': '6', 'Cantidad': '83500', 'Check': True}, {'id': 6, 'NombreCuenta': 'Caja', 'NumMovimiento': '7', 'Cantidad': '78000', 'Check': True}, {'id': 7, 'NombreCuenta': 'Caja', 'NumMovimiento': '8', 'Cantidad': '7000', 'Check': True}]
 Tipo="Inventarios Perpetuos"
-dataP = [['Balanza de comprobación',' ',' ',' ',' ',' '],
-    [' ',' ','Movimientos',' ','Saldo',' '],
-    ['No','Cuenta ','Deudor','Acreedor','Deudor','Acreedor']]
+dataP = [['Estado de resultados',' ',' ',' ',' ']]
 ISR=.36
 PTU=.10
-CuentasOAP = ['Inventario','Compras',"Gasto de Compra","Devoluciones sobre compra","Rebajas sobre compra","Ventas","Devoluciones sobre venta","Rebajas sobre venta",'Gastos de venta','Gastos de administración','Gastos financieros','Productos financieros','Otros gastos','Otros productos']
-CuentasOIP = ['Almacen','Ventas','Costo de ventas','Gastos de venta','Gastos de administración','Gastos financieros','Productos financieros','Otros gastos','Otros productos']
+CuentasOAP = ['Inventario','Compras',"Gasto de Compra","Devoluciones sobre compra","Rebajas sobre compra","Ventas","Devoluciones sobre venta","Rebajas sobre venta",'Gasto de venta','Gasto de administración','Gastos financieros','Productos financieros','Otros gastos','Otros productos']
+CuentasOIP = ['Almacen','Ventas','Costo de ventas','Gasto de venta','Gasto de administración','Gastos financieros','Productos financieros','Otros gastos','Otros productos']
 JSONCuentasC = []
 BanderaI=1
 BanderaF=1
@@ -21,7 +19,7 @@ if(Tipo=='Analítico o Pormenorizado'):
                 JSONCuentasC.append(JSONCuentas[j])
                 BanderaF=0
         if(BanderaF==1):
-            JSONCuentasC.append(CuentasOAP[i])
+            JSONCuentasC.append({'id': 0, 'NombreCuenta': CuentasOAP[i]})
 elif(Tipo=='Inventarios Perpetuos'):
     for i in range (0,len(CuentasOIP)):
         BanderaF=1
@@ -30,7 +28,7 @@ elif(Tipo=='Inventarios Perpetuos'):
                 JSONCuentasC.append(JSONCuentas[j])
                 BanderaF=0
         if(BanderaF==1):
-            JSONCuentasC.append(CuentasOIP[i])
+            JSONCuentasC.append({'id': 0, 'NombreCuenta': CuentasOIP[i]})
 CantidadesOrdenadas= []
 for i in range (0,len(JSONCuentasC)):
     NombreCuenta=JSONCuentasC[i]["NombreCuenta"]
@@ -50,7 +48,7 @@ for i in range (0,len(JSONCuentasC)):
     else:
         CantidadesOrdenadas.append(MCantidadDer-MCantidadIzq)
 #Generar datos
-ArregloCuentas=["Ventas netas","Compras totales","Compras netas","Total de mercancias","Costo de ventas","bruta","de operación","neta"]
+ArregloCuentas=["Ventas netas","Compras totales","Compras netas","Total de mercancias","Costo de ventas","bruta","de operación","neta","ISR (36%)","PTU (10%)","F"]
 ArregloCuentasValores=[]
 if(Tipo=='Analítico o Pormenorizado'):
     ArregloCuentasValores.append(CantidadesOrdenadas[5]-(CantidadesOrdenadas[6]+CantidadesOrdenadas[7]))
@@ -59,24 +57,164 @@ if(Tipo=='Analítico o Pormenorizado'):
     ArregloCuentasValores.append(ArregloCuentasValores[2]+InventarioInicial)
     ArregloCuentasValores.append(ArregloCuentasValores[3]-CantidadesOrdenadas[0])
     if(ArregloCuentasValores[0]-ArregloCuentasValores[4]>0):
-        ArregloCuentas[5]=="Utilidad bruta"
+        ArregloCuentas[5]="Utilidad bruta"
     else:
-        ArregloCuentas[5]=="Perdida bruta"
+        ArregloCuentas[5]="Perdida bruta"
     ArregloCuentasValores.append(ArregloCuentasValores[0]-ArregloCuentasValores[4])
     ##############################################################
     if(ArregloCuentasValores[5]-CantidadesOrdenadas[8]-CantidadesOrdenadas[9]-CantidadesOrdenadas[10]+CantidadesOrdenadas[11]>0):
-        ArregloCuentas[6]=="Utilidad de operación"
+        ArregloCuentas[6]="Utilidad de operación"
     else:
-        ArregloCuentas[6]=="Perdida de operacion"
+        ArregloCuentas[6]="Perdida de operacion"
     ArregloCuentasValores.append(ArregloCuentasValores[5]-CantidadesOrdenadas[8]-CantidadesOrdenadas[9]-CantidadesOrdenadas[10]+CantidadesOrdenadas[11])
     ##############################################################
     if(ArregloCuentasValores[6]-CantidadesOrdenadas[12]+CantidadesOrdenadas[13]>0):
-        ArregloCuentas[7]=="Utilidad de neta"
+        ArregloCuentas[7]="Utilidad de neta"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[12]+CantidadesOrdenadas[13])
+        if(ArregloCuentasValores[7]-ArregloCuentasValores[7]*ISR-ArregloCuentasValores[7]*PTU>0):
+            ArregloCuentas[10]="Utilidad del ejercicio"
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*ISR)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*PTU)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]-ArregloCuentasValores[8]-ArregloCuentasValores[9])
+        else:
+            ArregloCuentas[10]="Perdida del ejercicio"
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*ISR)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*PTU)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]-ArregloCuentasValores[8]-ArregloCuentasValores[9])
     else:
-        ArregloCuentas[7]=="Perdida de neta"
-    ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[12]+CantidadesOrdenadas[13])
+        ArregloCuentas[7]="Perdida de neta"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[12]+CantidadesOrdenadas[13])
+        ArregloCuentas[10]="Perdida del ejercicio"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[12]+CantidadesOrdenadas[13])
 elif(Tipo=='Inventarios Perpetuos'):
+    ArregloCuentasValores.append(CantidadesOrdenadas[1])
+    ArregloCuentasValores.append("NO")
+    ArregloCuentasValores.append("NO")
+    ArregloCuentasValores.append("NO")
+    ArregloCuentasValores.append(CantidadesOrdenadas[2])
+    if(ArregloCuentasValores[0]-ArregloCuentasValores[4]>0):
+        ArregloCuentas[5]="Utilidad bruta"
+    else:
+        ArregloCuentas[5]="Perdida bruta"
+    ArregloCuentasValores.append(ArregloCuentasValores[0]-ArregloCuentasValores[4])
+    ##############################################################
+    if(ArregloCuentasValores[5]-CantidadesOrdenadas[3]-CantidadesOrdenadas[4]-CantidadesOrdenadas[5]+CantidadesOrdenadas[6]>0):
+        ArregloCuentas[6]="Utilidad de operación"
 
+    else:
+        ArregloCuentas[6]="Perdida de operacion"
+    ArregloCuentasValores.append(ArregloCuentasValores[5]-CantidadesOrdenadas[3]-CantidadesOrdenadas[4]-CantidadesOrdenadas[5]+CantidadesOrdenadas[6])
+    ##############################################################
+    if(ArregloCuentasValores[6]-CantidadesOrdenadas[7]+CantidadesOrdenadas[8]>0):
+        ArregloCuentas[7]="Utilidad de neta"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[7]+CantidadesOrdenadas[8])
+        if(ArregloCuentasValores[7]-ArregloCuentasValores[7]*ISR-ArregloCuentasValores[7]*PTU>0):
+            ArregloCuentas[10]="Utilidad del ejercicio"
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*ISR)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*PTU)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]-ArregloCuentasValores[8]-ArregloCuentasValores[9])
+        else:
+            ArregloCuentas[10]="Perdida del ejercicio"
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*ISR)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]*PTU)
+            ArregloCuentasValores.append(ArregloCuentasValores[7]-ArregloCuentasValores[8]-ArregloCuentasValores[9])
+    else:
+        ArregloCuentas[7]="Perdida de neta"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[7]+CantidadesOrdenadas[8])
+        ArregloCuentas[10]="Perdida del ejercicio"
+        ArregloCuentasValores.append(ArregloCuentasValores[6]-CantidadesOrdenadas[7]+CantidadesOrdenadas[8])
+        ArregloCuentas.remove("ISR")
+        ArregloCuentas.remove("PTU")
+##########Formato tabla########### 
+banderaCostos=1
+for i in range(0,len(ArregloCuentas)):
+    if ArregloCuentas[i]=="Ventas netas":
+        dataP.append([ArregloCuentas[i]," ", " "," ",''.join(['$',str(ArregloCuentasValores[i])])])
+        dataP.append(["menos"," ", " "," "," "])
+    if ArregloCuentas[i]=="Costo de ventas":
+        dataP.append([ArregloCuentas[i]," ", " "," ",str(ArregloCuentasValores[i])])
+    if ArregloCuentas[i]=="Utilidad bruta" or ArregloCuentas[i]=="Perdida bruta":
+        if ArregloCuentas[i]=="Utilidad bruta":
+            dataP.append([ArregloCuentas[i]," ", " "," ",str(ArregloCuentasValores[i])])
+        else:
+            dataP.append([ArregloCuentas[i]," ", " "," ",''.join(['(',str(ArregloCuentasValores[i]*-1),')'])])
+        if(Tipo=='Analítico o Pormenorizado'):
+            if  CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')]>0:
+                if banderaCostos==1 and CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')]>0:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de venta'," ",''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')])])," "," "])
+                else:
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de venta'," ",''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')])]),''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')])])," "])
+            if  CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')]>0:
+                if banderaCostos==1:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de administración'," ",''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')])]),''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')])])," "])
+                else:
+                    dataP.append(['Gasto de administración',' ',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')]),''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')]+CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')])])," "])
+            if  CantidadesOrdenadas[CuentasOAP.index('Gastos financieros')]>0:
+                if banderaCostos==1:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gastos financieros'," ",""," ",''.join(['$',str(CantidadesOrdenadas[CuentasOAP.index('Gastos financieros')])])])
+                else:
+                    dataP.append(["mas"," ", " "," "," "])
+                    dataP.append(['Gastos financieros'," ", " ",str(CantidadesOrdenadas[CuentasOAP.index('Gastos financieros')]),str(CantidadesOrdenadas[CuentasOAP.index('Gasto de administración')]+CantidadesOrdenadas[CuentasOAP.index('Gasto de venta')]+CantidadesOrdenadas[CuentasOAP.index('Gastos financieros')])])
+        else:
+            if  CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')]>0:
+                if banderaCostos==1 and CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')]>0:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de venta'," ", ''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')])])," "," "])
+                else:
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de venta'," ", ''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')])]),''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')])])," "])
+            if  CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')]>0:
+                if banderaCostos==1:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gasto de administración'," ",''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')])]),''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')])])," "])
+                else:
+                    dataP.append(['Gasto de administración','',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')]),''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')]+CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')])]),""])
+            if  CantidadesOrdenadas[CuentasOIP.index('Gastos financieros')]>0:
+                if banderaCostos==1:
+                    banderaCostos=0
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Gastos financieros'," ",""," ",''.join(['$',str(CantidadesOrdenadas[CuentasOIP.index('Gastos financieros')])])])
+                else:
+                    dataP.append(["mas"," ", " "," "," "])
+                    dataP.append(['Gastos financieros'," ", " ",str(CantidadesOrdenadas[CuentasOIP.index('Gastos financieros')]),str(CantidadesOrdenadas[CuentasOIP.index('Gasto de administración')]+CantidadesOrdenadas[CuentasOIP.index('Gasto de venta')]+CantidadesOrdenadas[CuentasOIP.index('Gastos financieros')])])
+    if ArregloCuentas[i]=="Perdida de operacion" or ArregloCuentas[i]=="Utilidad de operación":
+        if ArregloCuentas[i]=="Utilidad de operación":
+            dataP.append([ArregloCuentas[i]," ", " "," ",str(ArregloCuentasValores[i])])
+        else:
+            dataP.append([ArregloCuentas[i]," ", " "," ",''.join(['(',str(ArregloCuentasValores[i]*-1),')'])])
+        if(Tipo=='Analítico o Pormenorizado'):
+            if  CantidadesOrdenadas[CuentasOAP.index('Otros gastos')]>0:
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Otros gastos'," ",""," ",str(CantidadesOrdenadas[CuentasOAP.index('Otros gastos')])])
+            if  CantidadesOrdenadas[CuentasOAP.index('Otros productos')]>0:
+                    dataP.append(["mas"," ", " "," "," "])
+                    dataP.append(['Otros productos'," ", " ","",str(CantidadesOrdenadas[CuentasOAP.index('Otros productos')])])
+        else:
+            if  CantidadesOrdenadas[CuentasOIP.index('Otros gastos')]>0:
+                    dataP.append(["menos"," ", " "," "," "])
+                    dataP.append(['Otros gastos'," ",""," ",str(CantidadesOrdenadas[CuentasOIP.index('Otros gastos')])])
+            if  CantidadesOrdenadas[CuentasOIP.index('Otros productos')]>0:
+                    dataP.append(["mas"," ", " "," "," "])
+                    dataP.append(['Otros productos'," ", " ","",str(CantidadesOrdenadas[CuentasOIP.index('Otros productos')])])
+    if ArregloCuentas[i]=="Perdida del ejercicio" or ArregloCuentas[i]=="Utilidad del ejercicio":
+        if "ISR" in ArregloCuentas:
+            dataP.append([ArregloCuentas[i-3]," ", " "," ",str(ArregloCuentasValores[i-3])])
+            dataP.append(["menos"," ", " "," "," "])
+            dataP.append([ArregloCuentas[i-2]," ", " ",str(ArregloCuentasValores[i-2]),""])
+            dataP.append([ArregloCuentas[i-1]," ", " ",str(ArregloCuentasValores[i-1]),str(ArregloCuentasValores[i-1]+ArregloCuentasValores[i-2])])
+            dataP.append([ArregloCuentas[i]," ", " "," ",str(ArregloCuentasValores[i])])
+        else:
+            dataP.append([ArregloCuentas[i]," ", " "," ",''.join(['(',str(ArregloCuentasValores[i]*-1),')'])])
+        
 fileName = 'EstadosdeResultados.pdf'
 
 from reportlab.platypus import SimpleDocTemplate
@@ -98,11 +236,11 @@ style = TableStyle([
     ('BACKGROUND', (0,0), (3,0), colors.white),
     ('TEXTCOLOR',(0,0),(-1,0),colors.black),
     ('ALIGN',(0,0),(-1,-1),'LEFT'),
-    ('ALIGN',(0,0),(5,0),'CENTER'),
+    ('ALIGN',(0,0),(4,0),'CENTER'),
     ('ALIGN',(2,len(dataP)-1),(-1,len(dataP)-1),'CENTER'),
     ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
     ('FONTSIZE', (0,0), (-1,0), 14),
-    ('FONTNAME', (0,1), (-1,1), 'Helvetica-Bold'),
+    ('FONTNAME', (0,1), (-1,1), 'Helvetica'),
 
     ('BOTTOMPADDING', (0,0), (-1,0), 12),
 
@@ -115,10 +253,10 @@ table.setStyle(style)
 ts = TableStyle(
     [
     ('BOX',(0,0),(-1,-1),1,colors.black),
-    ('LINEBEFORE',(2,1),(2,1),1,colors.black),
-    ('LINEBEFORE',(4,1),(4,1),1,colors.black),
+    #('LINEBEFORE',(2,1),(2,1),1,colors.black),
+    #('LINEBEFORE',(4,1),(4,1),1,colors.black),
     ('LINEABOVE',(0,1),(-1,1),1,colors.black),
-    ('GRID',(0,2),(-1,-1),1,colors.black),
+    ('GRID',(0,1),(-1,-1),1,colors.black),
     ]
 )
 table.setStyle(ts)
